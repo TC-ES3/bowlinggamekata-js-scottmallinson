5639e83c9db2605eb2f167e1b76358773f0fb6c8
Scott Mallinson <smallinson@PFO-R90VESRC-LAP.pasiona.es>
2019-11-15 15:03:39 +0100

    commit 7

--- bowling.spec.js
+++ bowling.spec.js
@@ -7,3 +7,10 @@ test("gutter game", () => {
         g.roll(0);
     expect(g.score()).toBe(0);
 });
+
+test("all ones", () => {
+    const g = new Game();
+    for (let i = 0; i < 20; i += 1)
+        g.roll(1);
+    expect(g.score()).toBe(20);
+})
