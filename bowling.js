432de439c505bd0ac4ce1f4575356d1212cc5aef
Scott Mallinson <smallinson@PFO-R90VESRC-LAP.pasiona.es>
2019-11-15 15:34:07 +0100

    commit 19

--- bowling.js
+++ bowling.js
@@ -1,12 +1,10 @@
 export default class Game {
-    _score = 0;
     _rolls = [];
 
     roll(pins) {
-        this._score += pins;
         this._rolls.push(pins);
     }
-    
+
     score() {
         let score = 0;
         for (let i = 0; i < this._rolls.length; i++) {
