5d8f452356986f56cb42f4784af8da0827a9fe84
Scott Mallinson <smallinson@PFO-R90VESRC-LAP.pasiona.es>
2019-11-15 16:21:38 +0100

    commit 30

--- bowling.js
+++ bowling.js
@@ -1,32 +1,43 @@
 export default class Game {
-    _rolls = [];
+  _rolls = [];
 
-    roll(pins) {
-        this._rolls.push(pins);
-    }
+  roll(pins) {
+    this._rolls.push(pins);
+  }
 
-    score() {
-        const rolls = this._rolls;
-        let score = 0;
-        let frameIndex = 0;
-        for (let frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] == 10) {
-                // strike
-                score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
-                frameIndex += 1;
-            }
-            else if (isSpare(rolls, frameIndex)) {
-                score += 10 + rolls[frameIndex + 2];
-                frameIndex += 2;
-            } else {
-                score +=  rolls[frameIndex] + rolls[frameIndex + 1]
-                frameIndex += 2;
-            }
-        }
-        return score;
+  score() {
+    const rolls = this._rolls;
+    let score = 0;
+    let frameIndex = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (rolls[frameIndex] == 10) {
+        // strike
+        score += 10 + strikeBonus(rolls, frameIndex);
+        frameIndex += 1;
+      } else if (isSpare(rolls, frameIndex)) {
+        score += 10 + spareBonus(rolls, frameIndex);
+        frameIndex += 2;
+      } else {
+        score += sumOfBallsInFrame(rolls, frameIndex);
+        frameIndex += 2;
+      }
     }
+    return score;
+  }
+}
+
+function strikeBonus(rolls, frameIndex) {
+  return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+}
+
+function sumOfBallsInFrame(rolls, frameIndex) {
+  return rolls[frameIndex] + rolls[frameIndex + 1];
+}
+
+function spareBonus(rolls, frameIndex) {
+  return rolls[frameIndex + 2];
 }
 
 function isSpare(rolls, frameIndex) {
-    return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+  return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
 }
