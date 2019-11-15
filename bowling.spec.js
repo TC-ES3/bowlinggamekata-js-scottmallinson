9db8f3bc19c74e74e9d51a70da73311bacb86cb8
Scott Mallinson <smallinson@PFO-R90VESRC-LAP.pasiona.es>
2019-11-15 15:20:20 +0100

    commit 14

--- bowling.spec.js
+++ bowling.spec.js
@@ -4,6 +4,11 @@ import Game from './bowling';
 let g;
 beforeEach(() => (g = new Game()));
 
+function rollMany(rolls, pins) {
+    for (let i = 0; i < rolls; i += 1)
+        g.roll(pins);
+}
+
 test("gutter game", () => {
     rollMany(20, 0);
     expect(g.score()).toBe(0);
@@ -13,8 +18,3 @@ test("all ones", () => {
     rollMany(20, 1);
     expect(g.score()).toBe(20);
 })
-
-function rollMany(rolls, pins) {
-    for (let i = 0; i < rolls; i += 1)
-        g.roll(pins);
-}
